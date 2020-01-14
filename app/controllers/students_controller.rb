9ecb546676d892b425865c184a259690f5a275b2
hylobates-lar <alison.v.quaglia@gmail.com>
2020-01-13 21:20:43 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,27 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    Student.create(student_params)
+
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
 
 end
